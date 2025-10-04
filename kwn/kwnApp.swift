99890783dadaa1e499754857b1476419a65495f5
John DeVito <john.devito@bulletproofsi.com>
2025-10-04 15:48:03 -0700

    Initial Commit

---
+++ kwn/kwnApp.swift
@@ -0,0 +1,17 @@
+//
+//  kwnApp.swift
+//  kwn
+//
+//  Created by john on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct kwnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
